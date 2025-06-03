e2ab76b3cd9e0701ec7d1e548b0c638de258a755
MateoPinedaBel <MateoPinedaBel@Outlook.com>
2025-06-03 11:04:15 -0500

    Feat se crea repositorio.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        int edadPersona;
+        int cantidadHijos;
+        int productosDisponibles;
+        int identificadorProducto;
+        int puntajeExamen;
+        int estrellasCalificacion;
+        int porcentajeBateria;
+        int cantidadLibrosBiblioteca;
+        int puntuacionVideojuego;
+
+        long poblacionMundial;
+        long cantidadVisitantesSitioWeb;
+
+        double precioProducto;
+        double salarioTrabajador;
+        double distanciaCiudades;
+        double temperaturaAmbiente;
+        double velocidadAuto;
+        double precioBilleteAvion;;
+        double calificacionPromedioCurso;
+        double puntajePromedioDeportista;
+        double tipoCambioDivisas;
+        double volumenRecipiente;
+        double pesoPersona;
+        double notaFinalCurso;
+        double constantePlanck;
+
+        float alturaPersona;
+
+        boolean interruptorEncendido;
+        boolean esMayorDeEdad;
+        boolean puertaAbierta;
+        boolean archivoActivo;
+        boolean tieneLicenciaConduccion;
+        boolean sensorFuncionando;
+        boolean clienteRegistrado;
+        boolean pagoConfirmado;
+
+        char generoPersona;
+        char letraExamen;
+        char caracterEspecial;
+        char teclaPulsada;
+
+        String matriculaVehiculo;
+        String numeroTelefono;
+        String codigoPostal;
+        String nombreCompletoPersona;
+        String codigoBarrasProducto;
+        String nombreUsuarioApp;
+        String codigoProducto;
+        String nombreEmpresa;
+        String identificadorCliente;
+
+        LocalDate fechaNacimiento;
+
+        LocalTime horaEvento;
+        
+    }
+}
